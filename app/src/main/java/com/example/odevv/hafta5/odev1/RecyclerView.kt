8851cd0521f8affc5fd664bbe19f22ed57cbc669
ahmet <fenerhab@gmail.com>
2023-08-18 21:13:03 +0300

    .

---
+++ app/src/main/java/com/example/odevv/hafta5/odev1/RecyclerView.kt
@@ -0,0 +1,4 @@
+package com.example.odevv.hafta5.odev1
+
+//1.ImageView ve TextView içeren bir nesne sınıfı tanımlayın. RecyclerView içerisinde 3 lü ve yatayda scroll edilebilecek şekilde kodlayınız.
+//https://github.com/ahmetbgdy9/RecyclerView_Proje
